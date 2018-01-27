5fe80d3d6b8397d0162192538218f8f66966c2a3
Victor Aluko <victor.aluko@gmail.com>
2018-01-27 14:11:35 +0200

    Added mock backend for registration and log

--- index.js
+++ index.js
@@ -8,6 +8,16 @@ app.get('/', function(request, response) {
   response.send('Hello World!')
 })
 
+app.post('/api/register', function (request, response) {
+  response.setHeader('Content-Type', 'application/json');
+  response.send(JSON.stringify({ response: "success" }));
+})
+
+app.post('/api/login', function (request, response) {
+  response.setHeader('Content-Type', 'application/json');
+  response.send(JSON.stringify({ token: "123456", response: "success" }));
+})
+
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
