bb868bf83913bb0c79ecc12e7ac94f3c6c92e40c
John Ajiboye <ajiboye.o.john@gmail.com>
2018-03-09 17:39:13 +0200

    Updated

--- index.js
+++ index.js
@@ -11,13 +11,7 @@ app.use(cors());
 
 app.use(bodyParser.json()); // for parsing application/json
 
-var connection = mysqlConn.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'btex_currency',
-  port: 3307,
-});
+var connection = mysqlConn.createConnection(process.env.CLEARDB_DATABASE_URL);
 
 app.set('port', process.env.PORT || 5000);
 app.use(express.static(__dirname + '/public'));
